ce14fe54c6064c391a2a4e1138752eda89fd9781
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-03 10:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
-function sayhappyHolidays() {
+function happyHolidays() {
 return "Happy holidays!"}
 
-function sayhappyHolidaysto(name) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`}
 
 
